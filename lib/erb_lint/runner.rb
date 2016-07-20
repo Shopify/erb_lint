023d77d5519ec6e13c99f1411a0f45581a26bbc3
Justin Chan <justin.the.c@gmail.com>
2016-07-20 00:38:37 -0400

    Add excluded files config option for linters

--- lib/erb_lint/runner.rb
+++ lib/erb_lint/runner.rb
@@ -7,18 +7,19 @@ module ERBLint
       @config = default_config.merge(config || {})
 
       LinterRegistry.load_custom_linters
-      @linters = LinterRegistry.linters.select { |linter| linter_enabled?(linter) }
+      @linters = LinterRegistry.linters.select { |linter_class| linter_enabled?(linter_class) }
       @linters.map! do |linter_class|
         linter_config = @config['linters'][linter_class.simple_name]
         linter_class.new(linter_config)
       end
     end
 
-    def run(file)
-      @linters.map do |linter|
+    def run(filename, file_content)
+      linters_for_file = @linters.select { |linter| !linter_excludes_file?(linter, filename) }
+      linters_for_file.map do |linter|
         {
           linter_name: linter.class.simple_name,
-          errors: linter.lint_file(file)
+          errors: linter.lint_file(file_content)
         }
       end
     end
@@ -30,6 +31,16 @@ module ERBLint
       linter_class_found = linter_classes[linter_class.simple_name]
       return false if linter_class_found.nil?
       linter_class_found['enabled'] || false
+    end
+
+    def linter_excludes_file?(linter, filename)
+      excluded_filepaths = linter['exclude'] || []
+      excluded_filepaths.each do |path|
+        if File.fnmatch?(path, filename)
+          return true
+        end
+      end
+      false
     end
 
     def default_config
