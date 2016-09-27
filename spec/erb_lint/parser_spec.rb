f4f938028f1b09cbfdee38aa98d750fb182fcf05
Justin Chan <justin.the.c@gmail.com>
2016-09-26 22:30:40 -0400

    Fixed all tests

--- spec/erb_lint/parser_spec.rb
+++ spec/erb_lint/parser_spec.rb
@@ -81,10 +81,10 @@ describe ERBLint::Parser do
         context 'when the erb tags contain content on one line' do
           let(:file) { '<% this is some content %>' }
 
-          it 'returns a text node consisting only of whitespace' do
+          it 'returns a text node consisting of the erb tag containing the single line of content' do
             expect(described_class.parse(file).children.size).to eq 2
             expect(described_class.parse(file).child.text?).to be_truthy
-            expect(described_class.parse(file).child.text.strip).to eq ''
+            expect(described_class.parse(file).child.text).to eq '<% this is some content %>'
             expect(described_class.parse(file).child.text.scan(/\n/).size).to eq 0
           end
         end
@@ -92,10 +92,10 @@ describe ERBLint::Parser do
         context 'when the erb tags contain multiple lines' do
           let(:file) { "<% \n \n \n %>" }
 
-          it 'returns a text node consiting only of whitespace and three newlines' do
+          it 'returns a text node consiting of the erb tag containing the whitespace and three newlines' do
             expect(described_class.parse(file).children.size).to eq 2
             expect(described_class.parse(file).child.text?).to be_truthy
-            expect(described_class.parse(file).child.text.strip).to eq ''
+            expect(described_class.parse(file).child.text).to eq "<% \n \n \n %>"
             expect(described_class.parse(file).child.text.scan(/\n/).size).to eq 3
           end
         end
@@ -103,16 +103,16 @@ describe ERBLint::Parser do
         context 'when the erb tags contain content on multiple lines' do
           let(:file) { <<~FILE.chomp }
             <%
-              line1
-              line2
-              line3
+            line1
+            line2
+            line3
             %>
           FILE
 
-          it 'returns a text node consiting only of whitespace and four newlines' do
+          it 'returns a text node consiting of the erb tag containing the lines of content and four newlines' do
             expect(described_class.parse(file).children.size).to eq 2
             expect(described_class.parse(file).child.text?).to be_truthy
-            expect(described_class.parse(file).child.text.strip).to eq ''
+            expect(described_class.parse(file).child.text).to eq "<%\nline1\nline2\nline3\n%>"
             expect(described_class.parse(file).child.text.scan(/\n/).size).to eq 4
           end
         end
@@ -120,60 +120,60 @@ describe ERBLint::Parser do
         context 'when the erb tags contain an erb start tag' do
           let(:file) { '<% <% %>' }
 
-          it 'returns a text node consisting only of whitespace' do
+          it 'returns a text node consisting of the erb tag containing the erb start tag' do
             expect(described_class.parse(file).children.size).to eq 2
             expect(described_class.parse(file).child.text?).to be_truthy
-            expect(described_class.parse(file).child.text.strip).to eq ''
+            expect(described_class.parse(file).child.text).to eq '<% <% %>'
           end
         end
 
         context 'when the erb tags appear after an erb start tag literal' do
-          let(:file) { '<%% <% has literal %>' }
+          let(:file) { '<%% <% after literal %>' }
 
-          it 'returns a text node consisting only of an erb start tag literal and whitespace' do
+          it 'returns a text node consisting of the erb start tag literal and erb tag' do
             expect(described_class.parse(file).children.size).to eq 2
             expect(described_class.parse(file).child.text?).to be_truthy
-            expect(described_class.parse(file).child.text.strip).to eq '<%%'
+            expect(described_class.parse(file).child.text.strip).to eq '<%% <% after literal %>'
           end
         end
 
         context 'when the erb tags contain an erb end tag literal' do
           let(:file) { '<% has literal %%> %>' }
 
-          it 'returns a text node consisting only of whitespace' do
+          it 'returns a text node consisting of the erb tag containing the erb end tag literal' do
             expect(described_class.parse(file).children.size).to eq 2
             expect(described_class.parse(file).child.text?).to be_truthy
-            expect(described_class.parse(file).child.text.strip).to eq ''
+            expect(described_class.parse(file).child.text.strip).to eq '<% has literal %%> %>'
           end
         end
 
         context 'when the erb tags are separated in strings' do
           let(:file) { "'<%=' + within_string + '%>'" }
 
-          it 'returns a text node consisting of single quotes containing 26 spaces' do
+          it 'returns a text node consisting of the string separated erb tags' do
             expect(described_class.parse(file).children.size).to eq 2
             expect(described_class.parse(file).child.text?).to be_truthy
-            expect(described_class.parse(file).child.text.strip).to eq "'                          '"
+            expect(described_class.parse(file).child.text.strip).to eq "'<%=' + within_string + '%>'"
           end
         end
 
         context 'when the erb tags contain an invalid string with double quotes' do
           let(:file) { '<% " %>' }
 
-          it 'returns a text node consisting only of whitespace' do
+          it 'returns a text node consisting of the erb tag containing the double quote' do
             expect(described_class.parse(file).children.size).to eq 2
             expect(described_class.parse(file).child.text?).to be_truthy
-            expect(described_class.parse(file).child.text.strip).to eq ''
+            expect(described_class.parse(file).child.text.strip).to eq '<% " %>'
           end
         end
 
         context 'when the erb tags contain an invalid string with single quotes' do
           let(:file) { "<% ' %>" }
 
-          it 'returns a text node consisting only of whitespace' do
+          it 'returns a text node consisting of the erb tag containing the single quote' do
             expect(described_class.parse(file).children.size).to eq 2
             expect(described_class.parse(file).child.text?).to be_truthy
-            expect(described_class.parse(file).child.text.strip).to eq ''
+            expect(described_class.parse(file).child.text.strip).to eq "<% ' %>"
           end
         end
       end
@@ -186,12 +186,12 @@ describe ERBLint::Parser do
             </div>
           FILE
 
-          it 'returns a div node with a text node consisting only of whitespace as its child' do
+          it 'returns a div node with a text node consisting of the erb tag' do
             expect(described_class.parse(file).children.size).to eq 2
             expect(described_class.parse(file).child.name).to eq 'div'
             expect(described_class.parse(file).child.children.size).to eq 1
             expect(described_class.parse(file).child.child.text?).to be_truthy
-            expect(described_class.parse(file).child.child.text.strip).to eq ''
+            expect(described_class.parse(file).child.child.text.strip).to eq '<% within element content %>'
           end
         end
 
@@ -200,23 +200,10 @@ describe ERBLint::Parser do
             <div class="<% within element attribute %>"></div>
           FILE
 
-          it 'returns a div node with a class atribute value consisting only of whitespace' do
+          it 'returns a div node with a class attribute value consisting of the erb tag' do
             expect(described_class.parse(file).children.size).to eq 2
             expect(described_class.parse(file).child.name).to eq 'div'
-            expect(described_class.parse(file).child['class'].strip).to eq ''
-          end
-        end
-
-        context 'when the erb tags are within the attribute list of the element' do
-          let(:file) { <<~FILE.chomp }
-            <div <% within element attribute list %>></div>
-          FILE
-
-          it 'returns a div node with no attributes or children' do
-            expect(described_class.parse(file).children.size).to eq 2
-            expect(described_class.parse(file).child.name).to eq 'div'
-            expect(described_class.parse(file).child.attributes.size).to eq 0
-            expect(described_class.parse(file).child.children.size).to eq 0
+            expect(described_class.parse(file).child['class'].strip).to eq '<% within element attribute %>'
           end
         end
       end
