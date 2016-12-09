583991ffcd5fcea79865a5063d688192ff92b3c3
nathanmarks <info@nathanmarks.io>
2016-12-09 09:52:44 -0500

    Fix capture block regex

--- lib/erb_lint/erb_parser.rb
+++ lib/erb_lint/erb_parser.rb
@@ -7,6 +7,7 @@ module ERBLint
       def parse(file_content)
         file_content = transform_capture_blocks(file_content)
         eruby = Erubi::CaptureEngine.new(file_content)
+        STDOUT.puts eruby.src
         Parser::CurrentRuby.parse(eruby.src)
       rescue Parser::SyntaxError
         raise ParseError, 'File is not valid ERB and could not be parsed.'
@@ -18,7 +19,7 @@ module ERBLint
       private
 
       def transform_capture_blocks(src)
-        regexp = /<%(={1,2})(([\s\S]*?)(\sdo\s)(\|.*\|\s)?%>)/
+        regexp = /<%(={1,2})([^%>]*?\sdo\s(?!\|.*\|\s)?%>)/
         src.gsub(regexp) { "<%|#{$1}#{$2}" }
       end
     end
--- spec/erb_lint/erb_parser_spec.rb
+++ spec/erb_lint/erb_parser_spec.rb
@@ -13,12 +13,20 @@ describe ERBLint::ERBParser do
     #   end
     # end
 
+    # context 'with an output including a capture block' do
+    #   let(:file) { File.read(File.expand_path('../fixtures/erb/example2.erb', __FILE__)) }
+
+    #   it 'returns a ruby ast' do
+    #     foo = described_class.parse(file)
+    #     expect(true).to eq true
+    #   end
+    # end
+
     context 'with an output including a capture block' do
-      let(:file) { File.read(File.expand_path('../fixtures/erb/example2.erb', __FILE__)) }
+      let(:file) { File.read(File.expand_path('../fixtures/erb/example3.erb', __FILE__)) }
 
       it 'returns a ruby ast' do
         foo = described_class.parse(file)
-        # binding.pry
         expect(true).to eq true
       end
     end
