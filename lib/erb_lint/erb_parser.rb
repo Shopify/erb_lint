736b9e879b33eb266188243d851a32151dc9f5f6
nathanmarks <info@nathanmarks.io>
2017-01-06 13:14:33 -0500

    Change capture block regex

--- lib/erb_lint/erb_parser.rb
+++ lib/erb_lint/erb_parser.rb
@@ -9,14 +9,14 @@ module ERBLint
         eruby = Erubi::CaptureEngine.new(file_content)
         Parser::CurrentRuby.parse(eruby.src)
       rescue Parser::SyntaxError
-        raise ParseError, 'File is not valid ERB and could not be parsed.'
+        raise ParseError, 'File could not be parsed.'
       end
 
       private
 
       def transform_capture_blocks(src)
-        regexp = /<%(={1,2})([^%>]*?\sdo\s(?:\|.*\|\s)?%>)/
-        src.gsub(regexp) { "<%|#{$1}#{$2}" }
+        regexp = /<%(={1,2}(?:(?!%>)[\s\S])+(?:\sdo\s(?:\|.*\|\s)?){1}%>)/
+        src.gsub(regexp) { "<%|#{$1}" }
       end
     end
 
--- spec/erb_lint/erb_parser_spec.rb
+++ spec/erb_lint/erb_parser_spec.rb
@@ -4,35 +4,44 @@ require 'spec_helper'
 
 describe ERBLint::ERBParser do
   describe '#parse' do
-    # context 'with a simple erb file' do
-    #   let(:file) { File.read(File.expand_path('../fixtures/erb/example1.erb', __FILE__)) }
+    context 'with a simple erb file' do
+      let(:file) { File.read(File.expand_path('../fixtures/erb/example1.erb', __FILE__)) }
 
-    #   it 'returns a ruby ast' do
-    #     foo = described_class.parse(file)
-    #     expect(true).to eq true
-    #   end
-    # end
-
-    # context 'with an output including a capture block' do
-    #   let(:file) { File.read(File.expand_path('../fixtures/erb/example2.erb', __FILE__)) }
-
-    #   it 'returns a ruby ast' do
-    #     foo = described_class.parse(file)
-    #     expect(true).to eq true
-    #   end
-    # end
-
-    # context 'with an output including multiple capture blocks' do
-    #   let(:file) { File.read(File.expand_path('../fixtures/erb/example3.erb', __FILE__)) }
-
-    #   it 'returns a ruby ast' do
-    #     foo = described_class.parse(file)
-    #     expect(true).to eq true
-    #   end
-    # end
+      it 'returns a ruby ast' do
+        foo = described_class.parse(file)
+        expect(true).to eq true
+      end
+    end
 
     context 'with an output including a capture block' do
+      let(:file) { File.read(File.expand_path('../fixtures/erb/example2.erb', __FILE__)) }
+
+      it 'returns a ruby ast' do
+        foo = described_class.parse(file)
+        expect(true).to eq true
+      end
+    end
+
+    context 'with an output including multiple capture blocks' do
+      let(:file) { File.read(File.expand_path('../fixtures/erb/example3.erb', __FILE__)) }
+
+      it 'returns a ruby ast' do
+        foo = described_class.parse(file)
+        expect(true).to eq true
+      end
+    end
+
+    context 'with an output including a nested capture block' do
       let(:file) { File.read(File.expand_path('../fixtures/erb/example4.erb', __FILE__)) }
+
+      it 'returns a ruby ast' do
+        foo = described_class.parse(file)
+        expect(true).to eq true
+      end
+    end
+
+    context 'with annoying characters in the args' do
+      let(:file) { File.read(File.expand_path('../fixtures/erb/example5.erb', __FILE__)) }
 
       it 'returns a ruby ast' do
         foo = described_class.parse(file)
