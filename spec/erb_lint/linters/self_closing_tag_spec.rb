10d999a86c433b79cd093c8d7f9a04c6f96185ba
Bernie Zang <bernie.zang@gmail.com>
2018-11-05 17:47:11 -0500

    test(self_closing_tag): Add tests

--- spec/erb_lint/linters/self_closing_tag_spec.rb
+++ spec/erb_lint/linters/self_closing_tag_spec.rb
@@ -3,7 +3,7 @@
 require 'spec_helper'
 
 describe ERBLint::Linters::SelfClosingTag do
-  let(:linter_config) { described_class.config_schema.new }
+  let(:linter_config) { described_class.config_schema.new(enforced_style: enforced_style) }
 
   let(:file_loader) { ERBLint::FileLoader.new('.') }
   let(:linter) { described_class.new(file_loader, linter_config) }
@@ -16,41 +16,87 @@ describe ERBLint::Linters::SelfClosingTag do
   describe 'offenses' do
     subject { offenses }
 
-    context 'when an element is not self-closing' do
-      let(:file) { "<a></a/>" }
-      it { expect(subject).to eq [] }
-    end
+    context 'when enforced_style is always' do
+      let(:enforced_style) { 'always' }
 
-    context 'when an element is self-closed correctly' do
-      let(:file) { "<br/>" }
-      it { expect(subject).to eq [] }
-    end
+      context 'when an element is not a void element' do
+        let(:file) { "<a></a/>" }
+        it { expect(subject).to eq [] }
+      end
 
-    context 'when an element is not #self_closing?' do
-      let(:file) { "<br>" }
-      it do
-        expect(subject).to eq [
-          build_offense(3..2, "Tag `br` is self-closing, it must end with `/>`."),
-        ]
+      context 'when a void element is #self-closed' do
+        let(:file) { "<br/>" }
+        it { expect(subject).to eq [] }
+      end
+
+      context 'when a void element is not #self_closing?' do
+        let(:file) { "<br>" }
+        it do
+          expect(subject).to eq [
+            build_offense(3..2, "Tag `br` is self-closing, it must end with `/>`."),
+          ]
+        end
+      end
+
+      context 'when a void element is #closing? and #self_closing?' do
+        let(:file) { "</br/>" }
+        it do
+          expect(subject).to eq [
+            build_offense(1..1, "Tag `br` is a void element, it must not start with `</`."),
+          ]
+        end
+      end
+
+      context 'when an element is #closing?' do
+        let(:file) { "</br>" }
+        it do
+          expect(subject).to eq [
+            build_offense(1..1, "Tag `br` is a void element, it must not start with `</`."),
+            build_offense(4..3, "Tag `br` is self-closing, it must end with `/>`."),
+          ]
+        end
       end
     end
 
-    context 'when an element is #closing? and #self_closing?' do
-      let(:file) { "</br/>" }
-      it do
-        expect(subject).to eq [
-          build_offense(1..1, "Tag `br` is self-closing, it must not start with `</`."),
-        ]
-      end
-    end
+    context 'when enforced_style is never' do
+      let(:enforced_style) { 'never' }
 
-    context 'when an element is #closing?' do
-      let(:file) { "</br>" }
-      it do
-        expect(subject).to eq [
-          build_offense(1..1, "Tag `br` is self-closing, it must not start with `</`."),
-          build_offense(4..3, "Tag `br` is self-closing, it must end with `/>`."),
-        ]
+      context 'when an element is not a void element' do
+        let(:file) { "<a></a/>" }
+        it { expect(subject).to eq [] }
+      end
+
+      context 'when a void element is #self-closed' do
+        let(:file) { "<br/>" }
+        it do
+          expect(subject).to eq [
+            build_offense(3..3, "Tag `br` is a void element, it must end with `>` and not `/>`."),
+          ]
+        end
+      end
+
+      context 'when a void element is not #self_closing?' do
+        let(:file) { "<br>" }
+        it { expect(subject).to eq [] }
+      end
+
+      context 'when a void element is #closing? and #self_closing?' do
+        let(:file) { "</br/>" }
+        it do
+          expect(subject).to eq [
+            build_offense(1..1, "Tag `br` is a void element, it must not start with `</`."),
+            build_offense(4..4, "Tag `br` is a void element, it must end with `>` and not `/>`."),
+          ]
+        end
+      end
+
+      context 'when an element is #closing?' do
+        let(:file) { "</br>" }
+        it do
+          expect(subject).to eq [
+            build_offense(1..1, "Tag `br` is a void element, it must not start with `</`."),
+          ]
+        end
       end
     end
   end
@@ -58,29 +104,62 @@ describe ERBLint::Linters::SelfClosingTag do
   describe 'autocorrect' do
     subject { corrected_content }
 
-    context 'when an element is not self-closing' do
-      let(:file) { "<a></a/>" }
-      it { expect(subject).to eq file }
+    context 'when enforced_style is always' do
+      let(:enforced_style) { 'always' }
+
+      context 'when an element is not self-closing' do
+        let(:file) { "<a></a/>" }
+        it { expect(subject).to eq file }
+      end
+
+      context 'when an element is self-closed' do
+        let(:file) { "<br/>" }
+        it { expect(subject).to eq file }
+      end
+
+      context 'when an element is not #self_closing?' do
+        let(:file) { "<br>" }
+        it { expect(subject).to eq "<br/>" }
+      end
+
+      context 'when an element is #closing? and #self_closing?' do
+        let(:file) { "</br/>" }
+        it { expect(subject).to eq "<br/>" }
+      end
+
+      context 'when an element is #closing?' do
+        let(:file) { "</br>" }
+        it { expect(subject).to eq "<br/>" }
+      end
     end
 
-    context 'when an element is self-closed correctly' do
-      let(:file) { "<br/>" }
-      it { expect(subject).to eq file }
-    end
+    context 'when enforced_style is never' do
+      let(:enforced_style) { 'never' }
 
-    context 'when an element is not #self_closing?' do
-      let(:file) { "<br>" }
-      it { expect(subject).to eq "<br/>" }
-    end
+      context 'when an element is not self-closing' do
+        let(:file) { "<a></a/>" }
+        it { expect(subject).to eq file }
+      end
 
-    context 'when an element is #closing? and #self_closing?' do
-      let(:file) { "</br/>" }
-      it { expect(subject).to eq "<br/>" }
-    end
+      context 'when an element is self-closed' do
+        let(:file) { "<br/>" }
+        it { expect(subject).to eq "<br>" }
+      end
 
-    context 'when an element is #closing?' do
-      let(:file) { "</br>" }
-      it { expect(subject).to eq "<br/>" }
+      context 'when an element is not #self_closing?' do
+        let(:file) { "<br>" }
+        it { expect(subject).to eq file }
+      end
+
+      context 'when an element is #closing? and #self_closing?' do
+        let(:file) { "</br/>" }
+        it { expect(subject).to eq "<br>" }
+      end
+
+      context 'when an element is #closing?' do
+        let(:file) { "</br>" }
+        it { expect(subject).to eq "<br>" }
+      end
     end
   end
 
