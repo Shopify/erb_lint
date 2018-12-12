941d5cdccbc4fad0a42322ec844d3891891348fd
bellzebu <bellzebu@hotmail.com>
2018-12-12 12:52:32 +0100

    Corrector changes
    - raises exception on initialize if diagnostics are present.
    - corrector memoization.
    - it has now the responsibility to write the corrected content to the linted file.
    - new method to update the status of the stored offenses when we run the client with the --autocorrect option.

--- lib/erb_lint/corrector.rb
+++ lib/erb_lint/corrector.rb
@@ -8,6 +8,8 @@ module ERBLint
       @processed_source = processed_source
       @offenses = offenses
       @corrected_content = corrector.rewrite
+
+      raise corrector.diagnostics.join(', ') if corrector.diagnostics.any?
     end
 
     def corrections
@@ -17,11 +19,25 @@ module ERBLint
     end
 
     def corrector
-      RuboCop::Cop::Corrector.new(@processed_source.source_buffer, corrections)
+      @corrector ||= RuboCop::Cop::Corrector.new(processed_source.source_buffer, corrections)
+    end
+
+    def update_offenses
+      @offenses.each do |offense|
+        offense.status = :corrected
+      end
     end
 
     def diagnostics
       corrector.diagnostics
+    end
+
+    def write
+      unless processed_source.file_content == corrected_content
+        File.open(processed_source.filename, "wb") do |file|
+          file.write(corrected_content)
+        end
+      end
     end
   end
 end
