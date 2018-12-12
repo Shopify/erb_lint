4e8770265a7a1b279bc36ae326be798d08fd4cf7
bellzebu <bellzebu@hotmail.com>
2018-12-12 12:41:15 +0100

    Client exit exceptions has been extracted to its own classes

---
+++ lib/erb_lint/cli/errors/exit_with_failure.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+module ERBLint
+  module Cli
+    module Errors
+      # Exit with failure error
+      class ExitWithFailure < RuntimeError; end
+    end
+  end
+end
---
+++ lib/erb_lint/cli/errors/exit_with_success.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+module ERBLint
+  module Cli
+    module Errors
+      # Exit with success error
+      class ExitWithSuccess < RuntimeError; end
+    end
+  end
+end
