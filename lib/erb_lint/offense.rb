39824d2d92fc2da31f64406c538caf98eb08860e
bellzebu <bellzebu@hotmail.com>
2018-12-12 12:31:25 +0100

    Added to_h method to Offense
    The Offense base class implements a to_h method with the offense details, this way we can use the resulting hash for multipourposes.
    Also, the offense is always initialized with an uncorrected status, the status has an attribute writter to change it to corrected when the client runs with the --autocorrect  option and has a new method to check if the offense was corrected or not.

--- lib/erb_lint/offense.rb
+++ lib/erb_lint/offense.rb
@@ -4,8 +4,9 @@ module ERBLint
   # Defines common functionality available to all linters.
   class Offense
     attr_reader :linter, :source_range, :message, :context
+    attr_writer :status
 
-    def initialize(linter, source_range, message, context = nil)
+    def initialize(linter, source_range, message, context = nil, status = :uncorrected)
       unless source_range.is_a?(Parser::Source::Range)
         raise ArgumentError, "expected Parser::Source::Range for arg 2"
       end
@@ -13,12 +14,31 @@ module ERBLint
       @source_range = source_range
       @message = message
       @context = context
+      @status = status
     end
+
+    def corrected
+      @status == :corrected
+    end
+    alias_method :corrected?, :corrected
 
     def inspect
       "#<#{self.class.name} linter=#{linter.class.name} "\
         "source_range=#{source_range.begin_pos}...#{source_range.end_pos} "\
         "message=#{message}>"
+    end
+
+    def to_h
+      {
+        linter: linter.class.simple_name,
+        corrected: linter.class.support_autocorrect? && corrected?,
+        source_range: "#{source_range.begin_pos}...#{source_range.end_pos}",
+        start_line: source_range.start_line,
+        start_column: source_range.start_column,
+        last_line: source_range.last_line,
+        length: source_range.length,
+        message: message,
+      }
     end
 
     def ==(other)
