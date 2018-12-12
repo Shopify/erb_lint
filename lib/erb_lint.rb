5e2f7e359feb8185842ed8edcea3c415e8b47b09
bellzebu <bellzebu@hotmail.com>
2018-12-12 13:21:15 +0100

    deps

--- lib/erb_lint.rb
+++ lib/erb_lint.rb
@@ -1,7 +1,14 @@
 # frozen_string_literal: true
 
+require 'erb_lint/cli/errors/exit_with_failure'
+require 'erb_lint/cli/errors/exit_with_success'
+require 'erb_lint/cli/opt_parser'
+require 'erb_lint/config_loader'
 require 'erb_lint/corrector'
 require 'erb_lint/file_loader'
+require 'erb_lint/formatters/formatter_factory'
+require 'erb_lint/formatters/default_formatter'
+require 'erb_lint/formatters/json_formatter'
 require 'erb_lint/linter_config'
 require 'erb_lint/linter_registry'
 require 'erb_lint/linter'
@@ -9,6 +16,8 @@ require 'erb_lint/offense'
 require 'erb_lint/processed_source'
 require 'erb_lint/runner_config'
 require 'erb_lint/runner'
+require 'erb_lint/stats'
+require 'erb_lint/utils/file_utils'
 require 'erb_lint/version'
 
 # Load linters
