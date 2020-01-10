e333f6c44945f048e7a98e4176c2c4f8cc56665b
Maple Ong <maple.ong@shopify.com>
2020-01-10 16:39:53 -0500

    Update outdated cop name

--- spec/erb_lint/linters/rubocop_spec.rb
+++ spec/erb_lint/linters/rubocop_spec.rb
@@ -207,12 +207,12 @@ describe ERBLint::Linters::Rubocop do
   context 'code is aligned to the column matching start of ruby code' do
     let(:linter_config) do
       described_class.config_schema.new(
-        only: ['Layout/AlignArguments'],
+        only: ['Layout/ArgumentAlignment'],
         rubocop_config: {
           AllCops: {
             TargetRubyVersion: '2.4',
           },
-          'Layout/AlignArguments': {
+          'Layout/ArgumentAlignment': {
             Enabled: true,
             EnforcedStyle: 'with_fixed_indentation',
             SupportedStyles: %w(with_first_parameter with_fixed_indentation),
@@ -244,7 +244,7 @@ describe ERBLint::Linters::Rubocop do
         expect(subject[0].source_range.source).to(eq("checked: true"))
         expect(subject[0].line_range).to(eq(2..2))
         expect(subject[0].message).to(\
-          eq("Layout/AlignArguments: Use one level of indentation for "\
+          eq("Layout/ArgumentAlignment: Use one level of indentation for "\
              "arguments following the first line of a multi-line method call.")
         )
       end
