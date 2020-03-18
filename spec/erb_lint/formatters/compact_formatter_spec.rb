b20ce7016d1de530332c4df4eea0755afcd4714f
Anderson Macedo <anderson.krs95@gmail.com>
2020-03-18 13:49:33 +0000

    Fix offenses

--- spec/erb_lint/formatters/compact_formatter_spec.rb
+++ spec/erb_lint/formatters/compact_formatter_spec.rb
@@ -19,11 +19,11 @@ describe ERBLint::Formatters::CompactFormatter do
                         column: 1),
       ]
     end
-    
+
     it "generates formatted offenses" do
       expect(subject).to(eq([
         'app/views/users/show.html.erb:61:10: Extra space detected where there should be no space.',
-        'app/views/users/show.html.erb:125:1: Remove multiple trailing newline at the end of the file.'
+        'app/views/users/show.html.erb:125:1: Remove multiple trailing newline at the end of the file.',
       ]))
     end
   end
