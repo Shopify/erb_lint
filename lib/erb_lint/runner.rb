fc2841da10bc34c5318eeb83abaf59ac56fcf3f6
Jen Taylor <jennifer.taylor@shopify.com>
2020-06-17 16:57:20 -0400

    Removed unecessary LinterRegistry call from runner

--- lib/erb_lint/runner.rb
+++ lib/erb_lint/runner.rb
@@ -10,7 +10,6 @@ module ERBLint
       @config = config || RunnerConfig.default
       raise ArgumentError, 'expect `config` to be a RunnerConfig instance' unless @config.is_a?(RunnerConfig)
 
-      LinterRegistry.load_custom_linters
       linter_classes = LinterRegistry.linters.select { |klass| @config.for_linter(klass).enabled? }
       @linters = linter_classes.map do |linter_class|
         linter_class.new(@file_loader, @config.for_linter(linter_class))
