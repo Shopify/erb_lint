5315f45c08e42b9649e140efafea1254b9717a58
Rafał Rothenberger <rafal.rothenberger@gmail.com>
2024-09-05 23:09:44 +0200

    Add NoneReporter

---
+++ lib/erb_lint/reporters/none_reporter.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module ERBLint
+  module Reporters
+    class NoneReporter < Reporter
+      def preview
+      end
+
+      def show
+      end
+    end
+  end
+end
